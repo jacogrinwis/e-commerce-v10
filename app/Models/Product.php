6d3e118f6bcb4f5ef197629ec73b75aad2781b7c
Jaco Grinwis <jacogrinwis@gmail.com>
2025-01-22 09:57:26 +0100

    Add models, migrations, and seeders for categories, colors, materials, and products

---
+++ app/Models/Product.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\StockStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'cover',
+        'price',
+        'discount',
+        'dimensions',
+        'weight',
+        'stock_status',
+        'category_id'
+    ];
+
+    // protected $casts = [
+    //     'stock_status' => StockStatus::class
+    // ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class);
+    }
+
+    public function materials()
+    {
+        return $this->belongsToMany(Material::class);
+    }
+
+    public function GetDiscountPriceAttribute()
+    {
+        if ($this->discount > 0) {
+            return $this->price * (1 - $this->discount / 100);
+        }
+        return $this->price;
+    }
+}
